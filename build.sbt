aad6caf00a3423ff3561103d9776ebf5a91c1f8a
a.kliusa <a.kliusa@ppl33-35.com>
2019-12-20 14:25:48 +0200

    first read of raw json :)

--- build.sbt
+++ build.sbt
@@ -2,10 +2,10 @@ name := "otusde201911hex5"
 
 version := "0.1"
 
-//scalaVersion := "2.13.1"
+scalaVersion := "2.11.12"
 
 //resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/maven"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
 libraryDependencies += "org.json4s" %% "json4s-native" % "3.6.7"
-
+libraryDependencies += "org.scala-lang" % "scala-xml" % "2.11.0-M4"
---
+++ src/main/scala/org/kliusa/otusde201911hex5/jsonreader/JsonReader.scala
@@ -0,0 +1,23 @@
+package org.kliusa.otusde201911hex5.jsonreader
+
+import org.apache.spark.sql.SparkSession
+
+object JsonReader extends App{
+
+  val jsonName = { if (args.length > 0) args(0) else "winemag-data-130k-v2.json" }
+
+  val sparkSession = SparkSession.builder().master("local").getOrCreate()
+  val sparkContext = sparkSession.sparkContext
+
+  val jsonRdd = sparkContext.textFile(jsonName)
+
+  println("Tolyan's json megaparser! :)")
+  println("JSON name: " + jsonName)
+  println("Top raw lines:")
+  println("------------------")
+  for ( s <- jsonRdd.top(5)) println(s)
+  println("------------------")
+
+
+
+}
