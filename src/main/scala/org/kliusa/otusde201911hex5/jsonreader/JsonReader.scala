1845a7bfde7717a26727872dc7f2e0e1bd82d412
tol2000 <tolicheg@gmail.com>
2019-12-22 23:15:56 +0200

    update

--- src/main/scala/org/kliusa/otusde201911hex5/jsonreader/JsonReader.scala
+++ src/main/scala/org/kliusa/otusde201911hex5/jsonreader/JsonReader.scala
@@ -1,6 +1,14 @@
+/*
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
+
+before - remove .sdk, .sbt, sdk and in .bashrc
+ */
+
 package org.kliusa.otusde201911hex5.jsonreader
 
-import org.apache.spark.Partitioner
 import org.apache.spark.sql.SparkSession
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
